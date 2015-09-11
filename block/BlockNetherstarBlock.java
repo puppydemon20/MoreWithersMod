bc8f0d195aafa2b1678f0ecca598a0e2a5de02ab
Dave Steed <puppydemon20@yahoo.com>
2015-09-11 01:23:14 -0700

    Slight Edit to NetherStar Block
    It now has a hardness and can only be broken by a diamond pick.

--- block/BlockNetherstarBlock.java
+++ block/BlockNetherstarBlock.java
@@ -2,15 +2,19 @@ package com.puppydemon.MoreWithersMod.block;
 
 import com.puppydemon.MoreWithersMod.reference.Reference;
 
+import net.minecraft.block.material.Material;
 import net.minecraft.util.IIcon;
 
 public class BlockNetherstarBlock extends BlockMWM
 {
 		public BlockNetherstarBlock()
 		{
-			super();
+			super(Material.iron);
 			this.setUnlocalizedName("NetherstarBlock");
 			this.setTextureName(Reference.MOD_ID + ":" + "NetherstarBlock");
+			this.setHardness(2.0f);
+	        this.setResistance(6.0f);
+	        this.setHarvestLevel("pickaxe", 2);
 			MultiTexture = false;
 		}
 		@Override
